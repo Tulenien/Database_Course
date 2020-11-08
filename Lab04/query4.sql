fb44ae2cbca49477ad7db99fe62e3bc5c83ecae2
Tulenien <timofey.evsigneev@gmail.com>
2020-11-08 22:29:32 +0300

    provide unique cad_num being generated

--- Lab04/query4.sql
+++ Lab04/query4.sql
@@ -1,15 +1,16 @@
 -- Хранимая процедура CLR
+-- Добавляет новые объекты недвижимости в таблицу
+-- Генерирует уникальный кадастровый номер самостоятельно
 
--- Проверка на копии таблицы re_obj
+-- Тестирование процедуры выполняется на копии таблицы re_obj
 select *
 into temp obj
 from re_obj;
 
-drop procedure addObj;
+--drop procedure addObj;
 
 create or replace procedure addObj
 (
-    cadn bigint,
     address varchar, 
     value integer,
     appdate date, 
@@ -17,14 +18,16 @@ create or replace procedure addObj
 )
 as
 $$
-#cadn = plpy.execute("select min(cad_num) cn from obj;")
-#cadn = cadn[0]["cn"]
-#cadn -= 1
+cadn = plpy.execute("select min(cad_num) cn from obj;")
+cadn = cadn[0]["cn"]
+cadn -= 1
 plan = plpy.prepare("insert into obj(cad_num, obj_address, cad_value, approval_date, obj_square) values($1, $2, $3, $4, $5);", ["bigint", "varchar", "bigint", "date", "smallint"])
 plpy.execute(plan, [cadn, address, value, appdate, square])
 $$ language plpython3u;
 
-call addObj(100615305002029, 'г.Москва *****', 26000000, '2002-10-09', 67);
+call addObj('г.Москва *****', 26000000, '2002-10-09', 67);
+call addObj('г.Москва *****', 26000000, '2002-10-09', 68);
+call addObj('г.Москва *****', 26000000, '2002-10-09', 69);
 
 select *
   from obj
