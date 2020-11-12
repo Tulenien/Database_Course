2e48a6cedf785258e0fd36cab64b68b48e36fe94
Tulenien <timofey.evsigneev@gmail.com>
2020-11-12 12:48:15 +0300

    Add TODO, start working on trigger

--- Lab03/queryC2.sql
+++ Lab03/queryC2.sql
@@ -18,3 +18,16 @@ create trigger DeletedFromOwners
 	instead of delete on owner_view
 	for each row 
 	execute procedure UpdateInsteadAgeZero()
+
+-- TODO: When smth is deleted from re_ownership -
+-- get all cad_nums and check owners
+-- if their number of properties - 1 = 0, then
+-- delete them in table. Check trigger on deletion
+-- Does it work on deletion under other triggers
+-- not connected to view or its own table
+create or replace function NoProperty()
+returns trigger as 
+$$
+begin
+	
+end;
