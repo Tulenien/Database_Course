522c1e6e8a80c9f15cb70475af0d05e62e844c05
Tulenien <timofey.evsigneev@gmail.com>
2020-11-20 00:41:10 +0300

    Add task three, modify json export to JSON-arrays, suitable for python json parser

--- Lab05/copyDBjson.sql
+++ Lab05/copyDBjson.sql
@@ -1,39 +1,34 @@
 copy
 (
-    select row_to_json(re_obj_data)
-    from
-    (
-        select * from re_obj
-    )re_obj_data
+    select jsonb_agg(reo)
+    from re_obj reo
 )
 to 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\re_objCopy.json';
 
 copy
 (
-    select row_to_json(re_ownership_data)
-    from
-    (
-        select * from re_ownership
-    )re_ownership_data
+    select jsonb_agg(reow)
+    from re_ownership reow
 )
 to 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\re_ownershipCopy.json';
 
 copy
 (
-    select row_to_json(re_owner_data)
-    from
-    (
-        select * from re_owner
-    )re_owner_data
+    select jsonb_agg(reo)
+    from re_owner reo
 )
 to 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\re_ownerCopy.json';
 
 copy
 (
-    select row_to_json(re_ownership_type_data)
-    from
-    (
-        select * from re_ownership_type
-    )re_ownership_type_data
+    select jsonb_agg(reot)
+    from re_ownership_type reot
 )
 to 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\re_ownership_typeCopy.json';
+
+copy
+(
+    select jsonb_agg(tns)
+    from tenants tns
+)
+to 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
