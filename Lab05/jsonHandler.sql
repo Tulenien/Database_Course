70f8497652cadcd27ea0979ccf99b9f9198e1d38
Tulenien <timofey.evsigneev@gmail.com>
2020-12-10 09:52:11 +0300

    Write last query

--- Lab05/jsonHandler.sql
+++ Lab05/jsonHandler.sql
@@ -77,3 +77,47 @@ select
         ) then 'Key exists'
 		else 'No such key'
 	end
+
+-- **********************
+-- Изменить json документ
+-- **********************
+create temp table import(doc json);
+copy import from 
+'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
+
+select *
+into temp tbl
+from
+(
+    select p.*
+    from import, json_populate_recordset(null::tenants, doc) as p
+) imp;
+
+select * from tbl;
+
+update tbl
+set info = info::jsonb || '{"rent":10}'::jsonb
+where id = 1;
+
+select info ->> 'rent' from tbl
+where info ->> 'name' = 'James';
+
+-- ***************************************************
+-- Разделить json документ на несколько строк по узлам
+-- ***************************************************
+create temp table import(doc json);
+copy import from 
+'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
+
+select *
+into temp tbl
+from
+(
+    -- Получить все, что внутри массива
+    select p.*
+    from import, json_array_elements(doc) p
+) imp;
+
+select json_each_text(tbl.value -> 'info') je
+from tbl
+order by je desc;
