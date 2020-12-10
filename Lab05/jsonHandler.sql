8b1db5e1465abc108e3bf08c664e2f757667fd89
Tulenien <timofey.evsigneev@gmail.com>
2020-12-10 07:57:30 +0300

    Finish key check query, fix style

--- Lab05/jsonHandler.sql
+++ Lab05/jsonHandler.sql
@@ -1,5 +1,6 @@
+-- ***************************************
 -- Извлечь json фрагмент из json документа
-
+-- ***************************************
 create temp table import(doc json);
 copy import from 
 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
@@ -16,7 +17,9 @@ from
 
 select * from tbl;
 
+-- *****************************************************
 -- Извлечь значения конкретных аттрибутов json документа
+-- *****************************************************
 create temp table import(doc json);
 copy import from 
 'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
@@ -33,22 +36,44 @@ from
 select json_extract_path(tbl.value, 'info', 'name') 
 from tbl;
 
--- Выполнить проверку существования узла или аттрибута
+-- **************************************************
+-- Выполнить проверку существования узла или атрибута
+-- **************************************************
+create temp table import(doc json);
+copy import from 
+'C:\Users\timof\Documents\Programming\DataBases\LabsDB\Lab05\tenantsCopy.json';
 
-with recursive get_keys(key, value) as 
+select *
+into temp tbl
+from
 (
-  select t.key, t.value
-  from tbl, json_each(tbl.value) t
-  union all
-  select t.key, t.value
-  from get_keys, json_each
-  (
-    case 
-      when json_typeof(get_keys.value) <> 'object' then '{}' :: json
-      else get_keys.value
-    end
-  ) as t
-)
-select distinct key
-from get_keys
-where json_typeof(get_keys.value) <> 'object';
+    -- Получить все, что внутри массива
+    select p.*
+    from import, json_array_elements(doc) p
+) imp;
+
+select
+	case 
+		when 'n' in 
+        (
+            -- Рекурсивный поиск всех ключей
+            with recursive get_keys(key, value) as 
+            (
+            select t.key, t.value
+            from tbl, json_each(tbl.value) t
+            union all
+            select t.key, t.value
+            from get_keys, json_each
+                (
+                    case 
+                    when json_typeof(get_keys.value) <> 'object' then '{}' :: json
+                    else get_keys.value
+                    end
+                ) t
+            )
+            select distinct key
+            from get_keys
+            where json_typeof(get_keys.value) <> 'object'
+        ) then 'Key exists'
+		else 'No such key'
+	end
