79c1ca98969ba8f1bfe8352bab0a91cdcd83062c
Tulenien <timofey.evsigneev@gmail.com>
2020-12-12 00:08:49 +0300

    Add second query function with multiple join, add messages on exit

--- Lab06/application.py
+++ Lab06/application.py
@@ -23,9 +23,11 @@ def getCursor(connection):
     return cursor
 
 def errorExit(connection):
+    print("Error. Changes aborted. Close connection")
     connection.rollback()
 
 def normalExit(connection):
+    print("Connection closed")
     connection.close()
 
 def getPriceByCadNum(CadNum, cursor):
@@ -42,10 +44,48 @@ def getPriceByCadNum(CadNum, cursor):
     try:
         cursor.execute("select cad_value from re_obj where cad_num = {}".format(CadNum))
         result = cursor.fetchall()
-        print(str(result) + "\n")
+        print(str(result[0]['cad_value']) + "\n")
+        return True
+    except:
+        return False
+
+def getFullInfoByCadNum(CadNum, cursor):
+    """Finds full information on the realty in
+    re_obj table of database.
+
+    Args:
+        CadNum (int): PK for realty object
+        cursor (cursor): DB cursor to execute queries through
+        
+    Returns:
+        flag: Indicator of execution
+    """
+    try:
+        cursor.execute\
+        (
+            '''select *
+            from re_obj reo
+            join re_ownership reow
+            on reo.cad_num = reow.cad_num
+            join re_owner ren
+            on reow.ensurance_num = ren.ensurance_num
+            join re_ownership_type reot
+            on reow.ownership_type_id = reot.ownership_type_id 
+            where reo.cad_num = {}'''.format(CadNum)
+        )
+        result = cursor.fetchall()
+        #print(result[0])
+        num = result[1]['ownership_type_id']
+        print(type(num))
         return True
     except:
         return False
 
 if __name__ == "__main__":
-    help(getPriceByCadNum)
+    connect = connectToRE()
+    if connect != None:
+        cursor = getCursor(connect)
+        # 824599521407922
+        getPriceByCadNum(824599521407922, cursor)
+        getFullInfoByCadNum(824599521407922, cursor)
+        normalExit(connect)
